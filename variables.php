ddda31fd7f2f01bc3d9ed917fbc1a61f153aae6c
V35HR4J <veshraj77@gmail.com>
2021-09-18 13:38:15 +0545

    day 1

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php 
+
+/// Variables in PHP Starts with $
+/// Variables are Containers for storing informations
+/// Variables are case Sensitive
+$name = "Veshraj";
+$income = 20000;
+echo "This is $name and his income is Rs. $income <br>";
+
+echo "$name is a good boy";
+
+
+
+?>
