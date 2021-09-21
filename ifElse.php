3da055118833771c01b1c13cb0e83172436df788
V35HR4J <veshraj77@gmail.com>
2021-09-21 21:20:10 +0545

    Day 4 - Ifelse

---
+++ ifElse.php
@@ -0,0 +1,38 @@
+<?php
+// $a = 665;
+// $b = 9;
+
+// if($a > 78){
+//     echo "a is greater than 78";
+// }
+// else{
+//     echo "a is not greater than 78";
+// }
+
+$age = 24;
+
+// If else ladder
+// if ($age>18){
+//     echo "You can drink water with chiya and alcohol";
+// }
+// elseif($age>13){
+//     echo "You can drink chiya only with water. No alcohol for you";
+// }
+// else{
+//     echo "You can drink water only";
+// }
+
+
+if ($age>18){
+    echo "You can drink water with chiya and alcohol<br>";
+}
+
+if($age>13){
+    echo "You can drink chiya only with water. No alcohol for you<br>";
+}
+else{
+    echo "You can drink water only<br>";
+}
+
+echo "Done";
+?>
